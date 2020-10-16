9d445a9bef8535f4d0e0a25099e675894f966883
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2020-10-16 10:39:49 +0530

    Delete Phone Number Tracker.py

--- Phone Number Tracker.py
+++
@@ -1,8 +0,0 @@
-import phonenumbers
-from phonenumbers import carrier
-from phonenumbers import geocoder
-
-ch_number=phonenumbers.parse("","CH")
-print(geocoder.description_for_number(ch_number,"de"))
-ro_number=phonenumbers.parse("","en")
-print(carrier.name_for_number(ro_number,"en"))
